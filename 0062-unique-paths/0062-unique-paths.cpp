28b8b1f389aefcd6062a67b5b762ba0d8b0c77af
SangamSangwan <sangamsangwan22@gmail.com>
2026-06-28 18:56:04 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int grid( vector<vector<int>> &dp,int m,int n)
-    {
-        if(m==0 || n==0) return 1;
-        if(dp[m][n]!=0) return dp[m][n];
-        else
-        {
-            dp[m][n]=grid(dp,m-1,n)+grid(dp,m,n-1);
-            return dp[m][n];
-        }
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n, 0));
-        return grid(dp,m-1,n-1);
-    }
+        int matrix [m][n];
+        for(int i=0; i<n; i++)
+        matrix[0][i]=1;
+        for (int i=0; i<m;i++)
+            matrix[i][0]=1;
+
+            for(int i=1; i<m; i++){
+                for(int j=1; j<n;j++){
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+                }
+            }
+            return matrix[m-1][n-1];
+        }
+        
+    
 };
