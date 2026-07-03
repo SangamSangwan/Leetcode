6873c0994e20559b8ed35e35fca385d82fdba2f6
SangamSangwan <sangamsangwan22@gmail.com>
2026-07-04 00:32:53 +0530

    Time: 17 ms (52.82%), Space: 76.3 MB (16.2%) - LeetHub

--- 2860-happy-students/2860-happy-students.cpp
+++ 2860-happy-students/2860-happy-students.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-   
     int countWays(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        int ways=0;
-        for(int i=0;i<nums.size();i++) 
-        {
-            if(i+1<nums.size())
-            {
-                if( nums[i]<i+1 && nums[i+1]>i+1) 
-                {
-                    ways++;    
-                }
-               
-            }
-            
+        sort(nums.begin() , nums.end());
+        
+        int ans = 0, n = nums.size() ;
+
+        
+        if(nums[0] > 0) ans++;
+        
+        for(int i = 0 ; i<nums.size()-1 ; i++){
+          
+           if( nums[i] <  i+1 && nums[i+1] > i+1)
+               ans++;
         }
-        if(nums[0]>0) ways++;
-        if(nums[nums.size()-1]<nums.size()) ways++;
-        return ways;
+        
+       
+        if(nums.back() < n) ans++;
+        
+        return ans;
     }
-    
 };
