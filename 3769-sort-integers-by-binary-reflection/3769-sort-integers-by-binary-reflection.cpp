f015b5d7ba2cfc0b56f25a96d41c2f2f05ba68dd
SangamSangwan <sangamsangwan22@gmail.com>
2026-07-05 01:37:56 +0530

    Time: 5 ms (62.66%), Space: 35.5 MB (35.71%) - LeetHub

---
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortByReflection(vector<int>& nums) {
+        vector<vector<int>> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int reflection = 0;
+            int temp = nums[i];
+
+            while (temp != 0) {
+                reflection = reflection * 2 + (temp & 1);
+                temp >>= 1;
+            }
+
+            s.push_back({reflection, nums[i]});
+        }
+
+        sort(s.begin(), s.end());
+
+        vector<int> ans;
+        for (auto &v : s) {
+            ans.push_back(v[1]);
+        }
+
+        return ans;
+    }
+};
