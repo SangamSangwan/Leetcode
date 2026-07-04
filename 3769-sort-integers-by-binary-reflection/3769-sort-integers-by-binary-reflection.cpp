35cde49912e008a92f3c2e9f2cda32ac81fe0432
SangamSangwan <sangamsangwan22@gmail.com>
2026-07-05 01:44:35 +0530

    Time: 4 ms (72.4%), Space: 33.5 MB (52.92%) - LeetHub

--- 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<int> sortByReflection(vector<int>& nums) {
-        vector<vector<int>> s;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int reflection = 0;
-            int temp = nums[i];
-
-            while (temp != 0) {
-                reflection = (reflection<<1) | (temp & 1);
-                temp >>= 1;
-            }
-
-            s.push_back({reflection, nums[i]});
+       if (nums.size() == 1) return nums;
+       vector<pair<int,int>> ans;
+       for(int i = 0; i <nums.size(); i++){
+        int sum = 0;
+        int n = nums[i];
+        while(n!=0){
+            sum = (sum << 1 )| (n&1);
+            n = n>>1;
         }
+        ans.push_back({sum,i});
+       }
 
-        sort(s.begin(), s.end());
+       sort(ans.begin(), ans.end(),
+    [&](const pair<int,int>& a, const pair<int,int>& b) {
+        if (a.first != b.first)
+            return a.first < b.first;          // reflection value
+        return nums[a.second] < nums[b.second]; // original value
+    });
 
-        vector<int> ans;
-        for (auto &v : s) {
-            ans.push_back(v[1]);
-        }
-
-        return ans;
+    vector<int> res;
+for (auto &p : ans) {
+    res.push_back(nums[p.second]);
+}
+       return res;
     }
 };
