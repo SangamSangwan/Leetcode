5997880679bac0a6c45b5e82e4ec56c116e86e00
SangamSangwan <sangamsangwan22@gmail.com>
2026-07-09 22:41:17 +0530

    Time: 51 ms (89.97%), Space: 60.4 MB (63.53%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,59 +1,11 @@
 class Solution {
 public:
-    
-    bool smaller(string &a, string &b) {
-        if (a.length() != b.length())
-            return a.length() < b.length();
-
-        return a < b;   
-    }
-
-    void merge(vector<string>& nums, int left, int mid, int right) {
-        int i = left;
-        int j = mid + 1;
-
-        vector<string> temp;
-
-        while (i <= mid && j <= right) {
-            if (smaller(nums[i], nums[j])) {
-                temp.push_back(nums[i]);
-                i++;
-            } else {
-                temp.push_back(nums[j]);
-                j++;
-            }
-        }
-
-        while (i <= mid) {
-            temp.push_back(nums[i]);
-            i++;
-        }
-
-        while (j <= right) {
-            temp.push_back(nums[j]);
-            j++;
-        }
-
-        for (int k = left; k <= right; k++) {
-            nums[k] = temp[k - left];
-        }
-    }
-
-    void mergeSort(vector<string>& nums, int left, int right) {
-        if (left >= right)
-            return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-
-        merge(nums, left, mid, right);
-    }
-
     string kthLargestNumber(vector<string>& nums, int k) {
-        mergeSort(nums, 0, nums.size() - 1);
-
-        return nums[nums.size() - k];
+       
+      sort(nums.begin() , nums.end() , [](string &a , string &b){
+        if(a.size() != b.size()) return a.size() > b.size();
+        return a > b;
+      });
+      return nums[k-1];
     }
 };
