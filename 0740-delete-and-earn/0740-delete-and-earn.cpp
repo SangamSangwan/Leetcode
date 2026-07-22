83e111b2e0fe2cb74286536eb5a6a4e51488c4e4
SangamSangwan <sangamsangwan22@gmail.com>
2026-07-22 23:48:07 +0530

    Time: 1 ms (53.28%), Space: 17.9 MB (21.41%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-
-    int solve(int i, vector<int>& points, vector<int>& dp)
-    {
-        if(i >= points.size())
-            return 0;
-
-        if(dp[i] != -1)
-            return dp[i];
-
-        
-        int take = points[i] + solve(i + 2, points, dp);
-
-        
-        int skip = solve(i + 1, points, dp);
-
-        return dp[i] = max(take, skip);
-    }
-
     int deleteAndEarn(vector<int>& nums) {
+        vector<int> profit(10007, 0);
+        for (int x: nums) profit[x] += x;
 
-        
-        vector<int> points(10001, 0);
+        int dp_prev = 0, dp_cur = 0;
+        for (int i = 1; i <= 10000; ++i)
+            dp_prev = std::exchange(dp_cur, max(dp_cur, profit[i] + dp_prev));
 
-        for(int x : nums)
-        {
-            points[x] += x;
-        }
-
-        vector<int> dp(10001, -1);
-
-        return solve(1, points, dp);
+        return dp_cur;
     }
 };
