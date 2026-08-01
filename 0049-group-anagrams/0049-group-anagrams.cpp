ecb4f0263cb43d8a91100ef11b422cda7713d075
SangamSangwan <sangamsangwan22@gmail.com>
2026-08-01 15:24:33 +0530

    Time: 15 ms (69.24%), Space: 25.1 MB (68.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
         unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+
+            string key = s;
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(s);
+        }
+
         vector<vector<string>> ans;
-        for(auto i : strs)
-        {
-            int freq[26]={0};
-            for(auto j:i)
-            {
-                freq[j-'a']++;
-            }
-            string s;
-            for(int k=0;k<26;k++)
-            {
-                s += to_string(freq[k]);
-                s+='#';
-            }
-            mp[s].push_back(i);
+
+        for (auto &x : mp) {
+            ans.push_back(x.second);
         }
-        for(auto it : mp)
-        {
-            ans.push_back(it.second);
-        }
+
         return ans;
     }
 };
